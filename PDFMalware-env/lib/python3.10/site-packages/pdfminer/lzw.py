0a524aaa6137082db74b006ae40b054f7e14bc99
justinecua <katsukiyu38@gmail.com>
2024-12-08 18:16:43 +0800

    update models

--- PDFMalware-env/lib/python3.10/site-packages/pdfminer/lzw.py
+++ PDFMalware-env/lib/python3.10/site-packages/pdfminer/lzw.py
@@ -1,6 +1,6 @@
-from io import BytesIO
 import logging
-
+from io import BytesIO
+from typing import BinaryIO, Iterator, List, Optional, cast
 
 logger = logging.getLogger(__name__)
 
@@ -10,33 +10,32 @@ class CorruptDataError(Exception):
 
 
 class LZWDecoder:
-
-    def __init__(self, fp):
+    def __init__(self, fp: BinaryIO) -> None:
         self.fp = fp
         self.buff = 0
         self.bpos = 8
         self.nbits = 9
-        self.table = None
-        self.prevbuf = None
-        return
+        # NB: self.table stores None only in indices 256 and 257
+        self.table: List[Optional[bytes]] = []
+        self.prevbuf: Optional[bytes] = None
 
-    def readbits(self, bits):
+    def readbits(self, bits: int) -> int:
         v = 0
         while 1:
             # the number of remaining bits we can get from the current buffer.
-            r = 8-self.bpos
+            r = 8 - self.bpos
             if bits <= r:
                 # |-----8-bits-----|
                 # |-bpos-|-bits-|  |
                 # |      |----r----|
-                v = (v << bits) | ((self.buff >> (r-bits)) & ((1 << bits)-1))
+                v = (v << bits) | ((self.buff >> (r - bits)) & ((1 << bits) - 1))
                 self.bpos += bits
                 break
             else:
                 # |-----8-bits-----|
                 # |-bpos-|---bits----...
                 # |      |----r----|
-                v = (v << r) | (self.buff & ((1 << r)-1))
+                v = (v << r) | (self.buff & ((1 << r) - 1))
                 bits -= r
                 x = self.fp.read(1)
                 if not x:
@@ -45,25 +44,25 @@ class LZWDecoder:
                 self.bpos = 0
         return v
 
-    def feed(self, code):
-        x = b''
+    def feed(self, code: int) -> bytes:
+        x = b""
         if code == 256:
             self.table = [bytes((c,)) for c in range(256)]  # 0-255
             self.table.append(None)  # 256
             self.table.append(None)  # 257
-            self.prevbuf = b''
+            self.prevbuf = b""
             self.nbits = 9
         elif code == 257:
             pass
         elif not self.prevbuf:
-            x = self.prevbuf = self.table[code]
+            x = self.prevbuf = cast(bytes, self.table[code])  # assume not None
         else:
             if code < len(self.table):
-                x = self.table[code]
-                self.table.append(self.prevbuf+x[:1])
+                x = cast(bytes, self.table[code])  # assume not None
+                self.table.append(self.prevbuf + x[:1])
             elif code == len(self.table):
-                self.table.append(self.prevbuf+self.prevbuf[:1])
-                x = self.table[code]
+                self.table.append(self.prevbuf + self.prevbuf[:1])
+                x = cast(bytes, self.table[code])
             else:
                 raise CorruptDataError
             table_length = len(self.table)
@@ -76,7 +75,7 @@ class LZWDecoder:
             self.prevbuf = x
         return x
 
-    def run(self):
+    def run(self) -> Iterator[bytes]:
         while 1:
             try:
                 code = self.readbits(self.nbits)
@@ -88,12 +87,17 @@ class LZWDecoder:
                 # just ignore corrupt data and stop yielding there
                 break
             yield x
-            logger.debug('nbits=%d, code=%d, output=%r, table=%r'
-                         % (self.nbits, code, x, self.table[258:]))
-        return
+
+            logger.debug(
+                "nbits=%d, code=%d, output=%r, table=%r",
+                self.nbits,
+                code,
+                x,
+                self.table[258:],
+            )
 
 
-def lzwdecode(data):
+def lzwdecode(data: bytes) -> bytes:
     fp = BytesIO(data)
     s = LZWDecoder(fp).run()
-    return b''.join(s)
+    return b"".join(s)
